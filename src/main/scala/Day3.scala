52be16e3c7b3651a3913a0519f14b52bc40aa149
oat9002 <oat9002@hotmail.com>
2020-12-13 09:25:09 +0700

    :construction: day3

---
+++ src/main/scala/Day3.scala
@@ -0,0 +1,33 @@
+import scala.io.Source
+
+object Day3 extends App {
+  val input = Source.fromResource("Day3Input.txt").getLines().toList
+
+  def countTrees(right: Int, down: Int): BigInt = {
+    var columnIdx = right
+    var rowIdx = down
+    var numOfTrees = 0
+
+    for (i <- input.indices) {
+      if (columnIdx >= input(i).length) {
+        columnIdx = columnIdx - input(i).length
+      }
+
+      if (rowIdx < input.length) {
+        val isTree = input(rowIdx)(columnIdx).equals('#')
+        numOfTrees = if (isTree) numOfTrees + 1 else numOfTrees
+      }
+
+      columnIdx = columnIdx + right
+      rowIdx = rowIdx + down
+    }
+
+    numOfTrees
+  }
+
+  countTrees(1, 2)
+  println(s"Result 1: ${countTrees(3, 1)}")
+  val result2: BigInt = countTrees(1, 1) * countTrees(3, 1) * countTrees(5, 1) * countTrees(7, 1) * countTrees(1, 2)
+
+  println(s"Result 2: $result2")
+}
