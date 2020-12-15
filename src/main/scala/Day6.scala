2593c6aaa167f6a2cc68ce4539cef70e976f98f8
oat9002 <oat9002@hotmail.com>
2020-12-16 01:02:29 +0700

    :construction: day6

---
+++ src/main/scala/Day6.scala
@@ -0,0 +1,41 @@
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Day6 extends App {
+  val input = Source.fromResource("Day6Input.txt").getLines()
+    .map(x => if (x.isEmpty) " " else x)
+    .reduce((acc, cur) => acc + cur)
+    .split(" ")
+
+  val result = input.map(countQuestion).sum
+
+  println(result)
+
+  val input2 = Source.fromResource("Day6Input.txt").getLines()
+    .map(x => if (x.isEmpty) "-" else x)
+    .reduce((acc, cur) => acc + " " + cur)
+    .split("-")
+    .map(_.trim)
+    .map(_.split(" "))
+
+  val result2 = input2.map(x => x.reduce((acc, cur) => acc.intersect(cur))).map(countQuestion).sum
+
+  println(result2)
+
+  def countQuestion(answer: String): Int = {
+    @tailrec
+    def getQuestions(ans: String, q: String = ""): String = {
+      if (ans.isEmpty) {
+        q
+      } else {
+        if (q.contains(ans.head)) {
+          getQuestions(ans.tail, q)
+        } else {
+          getQuestions(ans.tail, q + ans.head)
+        }
+      }
+    }
+
+    getQuestions(answer).length
+  }
+}
